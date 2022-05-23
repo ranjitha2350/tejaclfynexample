fbdb1282639a7c9dfeee735c55e1ba454e747490
ranjitha <ranjitha2350@gmail.com>
2022-05-24 00:31:06 +0530

    third commit

--- src/Sample.java
+++ src/Sample.java
@@ -5,11 +5,16 @@ public class Sample {
     public int sub(int a,int b){
         return a-b;
     }
+    public int mul(int a,int b){
+        return a*b;
+    }
     public static void main(String[] args) {
         Sample s = new Sample();
         int res = s.add(1,2);
         System.out.println(res);
         int res2 = s.sub(10,5);
         System.out.println(res2);
+        int res3 = s.mul(3,2);
+        System.out.println(res3);
     }
 }

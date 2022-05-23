76d4ce0ca70d6b86f123e5b7f348f52759ab5258
ranjitha <ranjitha2350@gmail.com>
2022-05-24 00:22:33 +0530

    second commit

--- src/Sample.java
+++ src/Sample.java
@@ -2,9 +2,14 @@ public class Sample {
     public int add(int a,int b){
         return a+b;
     }
+    public int sub(int a,int b){
+        return a-b;
+    }
     public static void main(String[] args) {
         Sample s = new Sample();
         int res = s.add(1,2);
         System.out.println(res);
+        int res2 = s.sub(10,5);
+        System.out.println(res2);
     }
 }
